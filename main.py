0fea23873524b4f6f351e880c6b14be132fb7af4
alexwoin <aleksei.aleksei0905@gmail.com>
2023-03-13 17:42:49 +0300

    Test

---
+++ main.py
@@ -0,0 +1,9 @@
+name1 = 'Процент'
+print(name1)
+name = '1 Процент'
+print(name)
+my_list = ['Процента', 'Процентов']
+for i in range(2, 5):
+    print(i, my_list[0])
+for i1 in range(5, 100):
+    print(i1, my_list[1])
